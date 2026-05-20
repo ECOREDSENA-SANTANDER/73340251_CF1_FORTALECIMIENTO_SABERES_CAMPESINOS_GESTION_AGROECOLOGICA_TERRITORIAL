dbafef39aadb46cc1d04a91846acfef14594617b
lizmanchego <karinamanchego0416@gmail.com>
2026-05-20 12:55:15 -0500

    [ADD] Tema 2 Full

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -6,26 +6,157 @@
       .titulo-principal__numero
         span 2
       h1 Territorio, ambiente y suelo
+    p.mb-0(data-aos='fade-right') El territorio, el ambiente y el suelo son elementos fundamentales en los sistemas productivos agropecuarios. Su comprensión permite analizar las condiciones donde se desarrollan las actividades agrícolas y tomar decisiones sostenibles que favorezcan la productividad y la conservación de los recursos naturales.
 
     Separador
     #t_2_1.titulo-segundo.color-acento-contenido(data-aos='fade-right')
       h2 2.1 Conocimiento campesino
 
+    .row.justify-content-center.mb-5(data-aos='fade-right')
+      .col-sm-12.col-lg-5.d-none.d-lg-block
+        img(src='@/assets/curso/temas/t2/1.png', alt='')
+      .col-sm-12.col-lg-7
+        .row.justify-content-center.mb-5.align-items-center
+          .col-sm-12.col-lg-2.d-none.d-lg-block
+            img(src='@/assets/curso/temas/t2/2.svg', alt='')
+          .col-sm-12.col-lg-10
+              p.mb-0 El conocimiento campesino es el conjunto de saberes, prácticas y experiencias que han sido construidas por las comunidades rurales a lo largo del tiempo. Este conocimiento se basa en la observación directa de la naturaleza, la experimentación y la transmisión oral entre generaciones.
+
+        p.mb-0 Su importancia radica en que permite desarrollar prácticas agrícolas adaptadas a las condiciones locales, como el clima, el tipo de suelo y la disponibilidad de recursos. Además, fortalece la identidad cultural y contribuye a la sostenibilidad de los sistemas productivos.
+
+
+    
+
     Separador
     #t_2_2.titulo-segundo.color-acento-contenido(data-aos='fade-right')
       h2 2.2 Territorio (dimensión ecológica, cultural y productiva)
+
+
+    .row.justify-content-center.mb-5(data-aos='fade-right')
+        .col-sm-12.col-lg-5.d-none.d-lg-block.mx-0.px-0
+          img(src='@/assets/curso/temas/t2/3.png', alt='')
+        .col-sm-12.col-lg-7.bg-color-3.p-4.br-3
+            img.wcus-2.mb-3(src='@/assets/curso/temas/t2/4.svg', alt='')
+            p.mb-3 El territorio es el espacio donde interactúan factores naturales, sociales y económicos. No solo se refiere al área geográfica, sino también a las relaciones que las comunidades establecen con su entorno.
+            ul.lista-ul--color.mb-0
+                li.d-flex.mb-0
+                  i.fas.fa-circle.icon-list
+                  p.mb-0 Dimensión ecológica: comprende los recursos naturales como suelo, agua, clima, flora y fauna, que condicionan la producción agropecuaria. 
+                li.d-flex.mb-0
+                  i.fas.fa-circle.icon-list
+                  p.mb-0 Dimensión cultural: incluye tradiciones, costumbres, saberes y formas de organización social de las comunidades. 
+                li.d-flex.mb-0
+                  i.fas.fa-circle.icon-list
+                  p.mb-0 Dimensión productiva: se refiere a las actividades económicas desarrolladas en el territorio, como la agricultura, la ganadería y otras formas de producción
+
+
+    .cajon.color-acento-contenido.p-4.mb-0(data-aos='fade-right')
+      .h5 Nota:
+      p.mb-0 Estas dimensiones están interrelacionadas y determinan el uso y manejo de los recursos.
+
+
+
 
     Separador
     #t_2_3.titulo-segundo.color-acento-contenido(data-aos='fade-right')
       h2 2.3 Agroecosistemas y ambiente
 
+
+    .row.justify-content-center.mb-5(data-aos='fade-right')
+      .col-sm-12.col-lg-5.d-none.d-lg-block
+        img(src='@/assets/curso/temas/t2/5.png', alt='')
+      .col-sm-12.col-lg-7
+          p.mb-0 Un agroecosistema es un sistema productivo donde interactúan componentes bióticos (plantas, animales, microorganismos) y abióticos (suelo, agua, clima), junto con la acción humana.
+          p.mb-5 El ambiente está conformado por todos los factores que influyen en estos sistemas. Las prácticas agropecuarias pueden generar impactos positivos, como la conservación de la biodiversidad, o negativos, como la contaminación, la deforestación y la degradación del suelo.
+          .cajon.color-secundario.p-4.mb-0
+            .h5 Nota:
+            p.mb-0 El enfoque agroecológico busca equilibrar estas interacciones para lograr sistemas productivos sostenibles.
+
+
+
+
+
+
     Separador
     #t_2_4.titulo-segundo.color-acento-contenido(data-aos='fade-right')
       h2 2.4 El suelo
+    p.mb-5(data-aos='fade-right') El suelo es un recurso natural esencial para la producción agrícola, ya que proporciona soporte, nutrientes y agua a las plantas.
+
+    .row.justify-content-center.mb-5(data-aos='fade-right')
+        .col-sm-12.col-lg-6.d-none.d-lg-block
+          img(src='@/assets/curso/temas/t2/6.png', alt='')
+        .col-sm-12.col-lg-6
+          LineaTiempoD.color-acento-botones.mb-0
+            .row.mb-0(numero="1" titulo="Uso del suelo")
+              .col-12
+                p.mb-0 Depende de su capacidad productiva y puede destinarse a agricultura, ganadería o conservación. 
+            .row.mb-0(numero="2" titulo="Degradación")
+              .col-12
+                p.mb-0 Ocurre por prácticas inadecuadas como la erosión, compactación, salinización y pérdida de materia orgánica. 
+            .row.mb-0(numero="3" titulo="Propiedades del suelo")
+              .col-12
+                ul.lista-ul--color.mb-0
+                  li.d-flex.mb-0
+                    i.fas.fa-circle.icon-list
+                    p.mb-0 #[b Físicas:] textura, estructura, porosidad. 
+                  li.d-flex.mb-0
+                    i.fas.fa-circle.icon-list
+                    p.mb-0 #[b Químicas:] pH, nutrientes, fertilidad. 
+                  li.d-flex.mb-0
+                    i.fas.fa-circle.icon-list
+                    p.mb-0 #[b Biológicas:] actividad de microorganismos. 
+
+            .row.mb-0(numero="4" titulo="Conservación")
+              .col-12
+                p.mb-0 Incluye prácticas como cobertura vegetal, rotación de cultivos y uso de abonos orgánicos. 
+            .row.mb-0(numero="5" titulo="Recuperación")
+              .col-12
+                p.mb-0 Acciones para restaurar suelos degradados, como la incorporación de materia orgánica y reforestación. 
+            .row.mb-0(numero="6" titulo="Manejo")
+              .col-12
+                p.mb-0 Conjunto de prácticas orientadas al uso sostenible del suelo, optimizando su productividad sin deteriorarlo. 
+
+
+
+
+
+
+
+
+
+
+
 
     Separador
     #t_2_5.titulo-segundo.color-acento-contenido(data-aos='fade-right')
       h2 2.5 Normativa agropecuaria en Colombia
+    p.mb-5(data-aos='fade-right') La normativa agropecuaria en Colombia establece lineamientos para el uso adecuado de los recursos, el acceso a la tierra y el desarrollo rural sostenible.
+    .row.justify-content-center.mb-5(data-aos='fade-right')
+        .col-sm-12.col-lg-3.d-none.d-lg-block.align-self-end
+          img(src='@/assets/curso/temas/t2/7.png', alt='')
+        .col-sm-12.col-lg-4.d-none.d-lg-block.mx-0.px-0
+          img(src='@/assets/curso/temas/t2/8.svg', alt='')
+        .col-sm-12.col-lg-5.bg-color-4.px-5.pt-5
+            p.mb-3 Entre las principales normas se destacan:
+            ul.lista-ul--color.mb-3
+                li.d-flex.mb-0
+                  i.fas.fa-leaf
+                  p.mb-0 #[b Constitución Nacional (Artículo 64):] promueve el acceso progresivo a la tierra para los trabajadores agrarios. 
+                li.d-flex.mb-0
+                  i.fas.fa-leaf
+                  p.mb-0 #[b Ley 160 de 1994:] regula la reforma agraria y el acceso a tierras. 
+                li.d-flex.mb-0
+                  i.fas.fa-leaf
+                  p.mb-0 #[b Ley 1876 de 2017:] crea el Sistema Nacional de Innovación Agropecuaria (SNIA). 
+                li.d-flex.mb-0
+                  i.fas.fa-leaf
+                  p.mb-0 #[b Ley 2219 de 2022:] impulsa la agroecología como modelo productivo sostenible. 
+                li.d-flex.mb-0
+                  i.fas.fa-leaf
+                  p.mb-0 #[b Ley 2294 de 2023:] Plan Nacional de Desarrollo con enfoque en sostenibilidad rural. 
+                li.d-flex.mb-0
+                  i.fas.fa-leaf
+                  p.mb-0 #[b Resoluciones 464 de 2017 y 331 de 2024:] establecen lineamientos para el desarrollo agropecuario sostenible.
 
 </template>
 
