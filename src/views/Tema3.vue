5b00310479b022b1f0b004bbcb6e0b1c780c512a
lizmanchego <karinamanchego0416@gmail.com>
2026-05-20 13:12:01 -0500

    [ADD] Tema 3 Full

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -6,22 +6,183 @@
       .titulo-principal__numero
         span 3
       h1 Metodología Campesino a Campesino – CAC
+    .row.justify-content-center.mb-0.align-items-center(data-aos="fade-right")
+      .col-sm-12.col-lg-1.d-none.d-lg-block
+        img(src='@/assets/curso/temas/t3/1.svg', alt='')
+      .col-sm-12.col-lg-11
+          p.mb-0 La metodología Campesino a Campesino (CAC) es una estrategia de aprendizaje participativo que promueve el intercambio de conocimientos entre productores rurales. Se basa en la idea de que los campesinos aprenden mejor a partir de la experiencia de otros campesinos, fortaleciendo así la autonomía, la organización comunitaria y la adopción de prácticas sostenibles.
+
+
 
     Separador
     #t_3_1.titulo-segundo.color-acento-contenido(data-aos='fade-right')
       h2 3.1 Concepto, objetivos y principios
 
+
+    .row.justify-content-center.align-items-end.mb-5(data-aos="fade-right")
+      .col-sm-12.col-lg-8
+        AcordionA(tipo="b" clase-tarjeta="tarjeta bg-brown")(data-aos='fade-right')
+          .row.justify-content-center.mb-0.align-items-end(titulo="Concepto")
+            .col-12
+              p.mb-0 La metodología CAC es un enfoque educativo horizontal donde los agricultores comparten saberes, experiencias y prácticas productivas, sin depender exclusivamente del conocimiento técnico externo. Se centra en el aprendizaje práctico, la experimentación y la participación.
+              
+          .row.justify-content-center.mb-0.align-items-end(titulo="Objetivos")
+            .col-12
+              ul.lista-ul--color.mb-0
+                li.d-flex.mb-0
+                  i.fas.fa-circle.icon-list
+                  p.mb-0  Fortalecer las capacidades de las comunidades rurales. 
+                li.d-flex.mb-0
+                  i.fas.fa-circle.icon-list
+                  p.mb-0 Promover prácticas agroecológicas sostenibles. 
+                li.d-flex.mb-0
+                  i.fas.fa-circle.icon-list
+                  p.mb-0 Facilitar el intercambio de conocimientos locales. 
+                li.d-flex.mb-0
+                  i.fas.fa-circle.icon-list
+                  p.mb-0 Impulsar la innovación desde el territorio. 
+                li.d-flex.mb-0
+                  i.fas.fa-circle.icon-list
+                  p.mb-0 Mejorar la producción y la calidad de vida de los campesinos. 
+          .row.justify-content-center.mb-0.align-items-end(titulo="Principios")
+            .col-12
+              ul.lista-ul--color.mb-0
+                li.d-flex.mb-0
+                  i.fas.fa-circle.icon-list
+                  p.mb-0 Horizontalidad: todos enseñan y aprenden. 
+                li.d-flex.mb-0
+                  i.fas.fa-circle.icon-list
+                  p.mb-0 Participación: la comunidad es protagonista. 
+                li.d-flex.mb-0
+                  i.fas.fa-circle.icon-list
+                  p.mb-0 Aprender haciendo: enfoque práctico. 
+                li.d-flex.mb-0
+                  i.fas.fa-circle.icon-list
+                  p.mb-0 Solidaridad: apoyo mutuo entre productores. 
+                li.d-flex.mb-0
+                  i.fas.fa-circle.icon-list
+                  p.mb-0 Respeto por el saber local: valorización del conocimiento campesino. 
+                li.d-flex.mb-0
+                  i.fas.fa-circle.icon-list
+                  p.mb-0 Sostenibilidad: prácticas amigables con el ambiente. 
+
+
+      .col-sm-12.col-lg-4.d-none.d-lg-block
+        img(src='@/assets/curso/temas/t3/2.png', alt='')
+
+
+
+
+
+
+
+
+
+
     Separador
     #t_3_2.titulo-segundo.color-acento-contenido(data-aos='fade-right')
       h2 3.2 Roles
 
+    p.mb-5(data-aos="fade-right") En los procesos de formación y desarrollo comunitario, especialmente dentro de la metodología Campesino a Campesino (CAC), los roles representan las funciones y responsabilidades que asumen los diferentes actores involucrados. Estos roles son fundamentales para garantizar la organización, participación y cumplimiento de los objetivos propuestos.
+
+
+    .tarjeta--container.row.mb-5(data-aos='fade-right')
+      .col-md.tarjeta.color-primario.p-5
+        .row.justify-content-center.mb-4
+          .col-6
+            figure
+              img(src='@/assets/curso/temas/t3/3.svg')         
+        h2.text-center Facilitador
+        p.mb-0 Es la persona que orienta el proceso de aprendizaje. Su función no es imponer conocimientos, sino guiar, motivar y promover la participación de la comunidad.
+
+
+      .col-md.tarjeta.color-acento-contenido.p-5
+        .row.justify-content-center.mb-4
+          .col-6
+            figure
+              img(src='@/assets/curso/temas/t3/4.svg')        
+        h2.text-center Promotor
+        p.mb-0 Generalmente es un líder campesino que comparte sus experiencias y buenas prácticas. Actúa como ejemplo dentro de la comunidad y facilita la adopción de nuevas tecnologías o prácticas.
+      
+      .col-md.tarjeta.color-primario.p-5
+        .row.justify-content-center.mb-4
+          .col-6
+            figure
+              img(src='@/assets/curso/temas/t3/5.svg')         
+        h2.text-center Coordinador
+        p.mb-0 Se encarga de la organización logística y seguimiento de las actividades. Articula a los participantes, gestiona recursos y asegura el cumplimiento de los objetivos.
+
+
+
+
+
     Separador
     #t_3_3.titulo-segundo.color-acento-contenido(data-aos='fade-right')
       h2 3.3 Estrategias pedagógicas participativas
+    p.mb-5(data-aos="fade-right") Son métodos de enseñanza que fomentan la participación activa, el diálogo y el aprendizaje colectivo. Se caracterizan por ser dinámicas, prácticas y adaptadas al contexto local.
+    .row.justify-content-center.mb-5.align-items-end(data-aos="fade-right")
+      .col-sm-12.col-lg-3.d-none.d-lg-block
+        img(src='@/assets/curso/temas/t3/6.png', alt='')
+      .col-sm-12.col-lg-9
+        .row.justify-content-center.mb-3
+          .col-sm-12.col-lg-6.d-none.d-lg-block.mx-0.px-0
+            img(src='@/assets/curso/temas/t3/7.svg', alt='')
+          .col-sm-12.col-lg-6.bg-color-4.p-5
+              p.mb-3 Entre las principales estrategias se encuentran:
+              ul.lista-ul--color.mb-0
+                  li.d-flex.mb-0
+                    i.fas.fa-circle.icon-list
+                    p.mb-0 Aprendizaje basado en la experiencia. 
+                  li.d-flex.mb-0
+                    i.fas.fa-circle.icon-list
+                    p.mb-0 Trabajo en grupo. 
+                  li.d-flex.mb-0
+                    i.fas.fa-circle.icon-list
+                    p.mb-0 Intercambio de conocimientos. 
+                  li.d-flex.mb-0
+                    i.fas.fa-circle.icon-list
+                    p.mb-0 Resolución de problemas reales. 
+                  li.d-flex.mb-0
+                    i.fas.fa-circle.icon-list
+                    p.mb-0 Observación y análisis en campo. 
+        .cajon.color-acento-contenido.p-4.mb-0
+          .h5 Nota:
+          p.mb-0 Estas estrategias permiten que el aprendizaje sea significativo y aplicable a la realidad de los participantes.
+
 
     Separador
     #t_3_4.titulo-segundo.color-acento-contenido(data-aos='fade-right')
       h2 3.4 Actividades
+    p.mb-5(data-aos="fade-right") Las actividades son el conjunto de acciones prácticas que permiten desarrollar los procesos de aprendizaje, participación e intercambio de conocimientos dentro de la comunidad. En la metodología Campesino a Campesino (CAC), las actividades constituyen el eje central del proceso formativo, ya que facilitan el aprendizaje mediante la experiencia directa y la interacción entre los participantes.
+
+    .row.justify-content-center.mb-5(data-aos='fade-right')
+        .col-sm-12.col-lg-6.d-none.d-lg-block
+          img(src='@/assets/curso/temas/t3/8.png', alt='')
+        .col-sm-12.col-lg-6
+          LineaTiempoD.color-acento-botones.mb-0
+            .row.mb-0(numero="1" titulo="Talleres")
+              .col-12
+                p.mb-0 Espacios de aprendizaje grupal donde se comparten conocimientos, se analizan problemas y se construyen soluciones de manera colectiva.
+            .row.mb-0(numero="2" titulo="Escuelas de campo (ECAS)")
+              .col-12
+                p.mb-0 Metodología de aprendizaje práctico en el campo, donde los participantes observan, experimentan y toman decisiones sobre el manejo de cultivos.
+            .row.mb-0(numero="3" titulo="Días de campo")
+              .col-12
+                p.mb-0 Eventos demostrativos donde se presentan resultados de prácticas agrícolas, permitiendo a otros productores observar y aprender directamente.
+            .row.mb-0(numero="4" titulo="Diagnósticos participativos")
+              .col-12
+                p.mb-0 Procesos en los que la comunidad analiza su realidad, identifica problemas y define acciones de mejora, fortaleciendo la toma de decisiones colectiva.
+
+
+
+
+
+
+
+
+
+
+
 
 </template>
 
